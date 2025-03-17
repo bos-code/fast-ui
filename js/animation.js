20d8581f1c2bea4872a1584b162c2af830314dd1
Henry chidera <chidera9713@gmail.com>
2025-03-17 05:05:15 +0100

    feat: feature section

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,22 @@
 import { gsap } from "gsap";
-    
+
 import { Flip } from "gsap/Flip";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Observer } from "gsap/Observer";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 
-
-gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,TextPlugin);
+gsap.registerPlugin(Flip, ScrollTrigger, Observer, ScrollToPlugin, TextPlugin);
 
 // gsap.to("h1", {
 //     duration: 1,
 //     x: 100,
 //     ease: "bounce"
 // });
+
+gsap.to(".bgi", {
+  duration: 10,
+  rotation: "+=360",
+  repeat: -1,
+  ease: "linear",
+});
