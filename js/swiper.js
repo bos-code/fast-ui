8f2a1453c8994c47b4dc1e2bc5bb4d49d582f3e5
Henry chidera <chidera9713@gmail.com>
2025-03-22 12:50:04 +0100

    feat: add swiper functionality and new images for gallery section

--- js/swiper.js
+++ js/swiper.js
@@ -1,2 +1,31 @@
 // import Swiper JS
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper";
+import "swiper/css/bundle";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2, // Show 2 slides at a time
+  //   centeredSlides: true, // Center the active slide
+  spaceBetween: 20, // Adjust spacing between slides
+  effect: "coverflow", // Nice 3D effect
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  grabCursor: true, // Makes dragging feel natural
+  loop: true, // Infinite scrolling
+  speed: 1000, // Animation speed
+});
+
+document.querySelector(".swiper-button-next").addEventListener("click", () => {
+  swiper.slideNext();
+});
+document.querySelector(".swiper-button-prev").addEventListener("click", () => {
+  swiper.slidePrev();
+});
